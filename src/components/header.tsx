0badac5eb8c8d799de36bf76e670534b2393f8fa
victorbuikem <chibuikemkalu466@gmail.com>
2025-03-21 05:40:21 +0100

    feat: update dependencies and enhance header component
    - Added new dependencies: @radix-ui/react-dialog, class-variance-authority, classnames, and lucide-react.
    - Updated package.json to reflect new dependencies and adjusted devDependencies.
    - Enhanced header component with mobile navigation using Sheet from Radix UI.
    - Improved global styles by adding border color and animation for better UI experience.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,12 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 import Image from "next/image";
-import logo from '../../public/logo.png'
 import Link from "next/link";
+import logo from '../../public/logo.png';
 
 const NAV_LINKS = [{
     text: 'Features',
@@ -16,17 +22,28 @@ const NAV_LINKS = [{
     url: '#dashboard'
 }]
 export function Header() {
-
     return (
-        <div className="mx-auto container px-12 py-3  flex justify-between items-center">
-            <Image src={logo} width={73} alt="BlockBatch" height={40} />
-            <nav className="flex gap-6">
-                {NAV_LINKS.map((value, index) => <NavigationItem key={`${index}_${Math.random()}`} {...value} />)}
-            </nav>
-            <div className="flex gap-2.5 items-center">
-                <button>Login</button>
-                <button className="px-4 py-2.5 bg-primary text-white rounded-md ">Get Started</button>
-            </div>
+        <div className="border-b-2 border-border">
+            <header className="mx-auto container px-4 md:px-12 py-3  flex justify-between items-center">
+                <Image src={logo} width={73} alt="BlockBatch" height={40} />
+                <nav className="hidden md:flex gap-6">
+                    {NAV_LINKS.map((value, index) => <NavigationItem key={`${index}_${Math.random()}`} {...value} />)}
+                </nav>
+                <div className=" hidden md:flex gap-2.5 items-center">
+                    <button className="font-medium text-sm">Login</button>
+                    <button className="px-4 py-2.5 bg-primary text-white rounded-md font-medium text-sm">Get Started</button>
+                </div>
+
+                {/* This is a sheet for Mobile Navigation */}
+                <Sheet>
+                    <SheetTrigger className="block md:hidden"><Menu /></SheetTrigger>
+                    <SheetContent>
+                        <nav className="flex flex-col gap-6 mt-8">
+                            {NAV_LINKS.map((value, index) => <NavigationItem key={`${index}_${Math.random()}`} {...value} />)}
+                        </nav>
+                    </SheetContent>
+                </Sheet>
+            </header>
         </div>
     )
 }
