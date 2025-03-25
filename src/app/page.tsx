5cee6105246b6f99935834a0654a8a2d9f97ebbf
DiegoB1911 <diego.barquero@hotmail.com>
2025-03-24 19:11:14 -0600

    feat: deleting unused image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import FeaturesOverviewSection from "./components/FeaturesOverviewSection";
 import { ProductDemo } from "@/components/product-demo";
-import Image from "next/image";
 import FinalCtaSection from "./components/FinalCtaSection";
 
 export default function Home() {
