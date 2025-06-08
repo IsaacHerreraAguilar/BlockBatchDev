fc226a082b9705bbdbc7a17a8008a14a17b3ec56
DiegoB1911 <diego.barquero@hotmail.com>
2025-06-08 16:00:57 -0600

    feat: adding google analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/app/components/header';
-
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 import Footer from "./components/footer/footer";
 
 const geistSans = Geist({
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-white`}
       >
+        <GoogleAnalytics />
         <Header />
         {children}
         <Footer />
